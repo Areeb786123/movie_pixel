fba5c660ec83e9c5ef828791fee49d6a1bec8c65
Areeb786123 <areebrockzz786@gmail.com>
2025-02-17 01:20:36 +0530

    grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
 import { Header } from './assets/header/header'
 import { Home } from './home/home'
+import { Home2 } from './home2/home';
 
 function App() {
   return (
     <div className="App bg-zinc-800">
       <Header />
       <Home />
+      <Home2/>
     </div>
   );
 
--- src/home2/home.jsx
+++ src/home2/home.jsx
@@ -1,14 +1,14 @@
 import HomeList from "../components/HomeList";
 import { useFetchMovies } from "../hooks/useFetchMovies";
 
-export const Home = () => {
+export const Home2 = () => {
     const { data, loading, error } = useFetchMovies()
     console.log(data)
     return (
-        <div className="bg-amber-300 w-full z-[1] h-full overflow-scroll" >
+        <div className="bg-black w-full z-[1] h-[100%] overflow-scroll">
             {loading && <p className="text-center">Loading...</p>}
             {error && <p className="text-red-500 text-center">{error}</p>}
-            <ul className="mt-4 flex-row flex flex-wrap gap-3 pb-2 justify-center">
+            <ul className="mt-4 flex-col flex space-y-2  pb-2">
                 {data && data.map((movie) => (
                     <HomeList movie={movie} key={"movie-id-" + movie.id} />
                 ))}
